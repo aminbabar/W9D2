bcc567a05e8aaa501cbba6a6a091c1b4b2cc4a59
Amin <amin.babar1@gmail.com>
2021-08-10 16:02:01 -0400

    after break

--- tictactoe/src/ttt-view.js
+++ tictactoe/src/ttt-view.js
@@ -1,12 +1,17 @@
+
+
 class View {
   constructor(game, el) {
     el.appendChild(this.setupBoard());
+    this.game = game;
     this.el = el;
+    // testing
+    this.bindEvents();
   }
 
   setupBoard() {
     const ul = document.createElement('ul');
-    for (let i = 1; i < 10; i++) {
+    for (let i = 0; i < 9; i++) {
       const li = document.createElement('li');
       li.classList.add(`${i}`, 'unclicked');
       // li.classList.add('unclicked');
@@ -16,15 +21,30 @@ class View {
   }
   // [[0,0]. [0, 1]...]
   bindEvents() {
+    
     let ul = document.querySelector('ul');
+    this.handleClick = this.handleClick.bind(this);
     ul.addEventListener('click', this.handleClick)
   }
 
   handleClick(e) {
-
+    let className = e.target.classList[0];
+    let square = e.target;
+    let positions = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2,0], [2,1], [2,2]]
+    // debugger;
+    square.innerText = this.game.currentPlayer;
+    this.game.playMove(positions[parseInt(className)]);
+    this.makeMove(square);
   }
 
-  makeMove(square) {}
+  makeMove(square) {
+    square.classList.add("clicked");
+    
+    // console.log(this.game.winner());
+    if (this.game.winner()) {
+      
+    }
+  }
 
 }
 
